5c7a20837a5b90066990fc83884a8870c6a78df7
D7703_22 <d7703_22@D7703-22ui-iMac-2.local>
2018-04-11 16:07:09 +0900

    d

--- Date Picker/ViewController.swift
+++ Date Picker/ViewController.swift
@@ -9,17 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myDatePicker: UIDatePicker!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var realTime: UILabel!
+    
+    var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func updateTime(){
+        let date=Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "현재시간:yyyy-MM-dd HH:mm:ss a EE"
+        realTime.text = formatter.string(from: date)
     }
-
-
+    
+    @IBAction func changeDatePicker(_ sender: Any) {
+        print("change Date Picker");
+        //print(myDatePicker.date)
+        
+        // dateFormat
+        let formatter = DateFormatter()
+        formatter.dateFormat="선택시간:yyyy-MM-dd HH:mm:ss a EE"
+        timeLabel.text=formatter.string(from: myDatePicker.date)
+        //timeLabel.text = String(describing: myDatePicker.date)
+    }
+    
 }
+
 
